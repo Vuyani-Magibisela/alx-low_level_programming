b793f11418f25d6f6c3e66565adfd7198f850984
vuyani-magibisela <vuyani.magibisela@live.com>
2022-07-08 17:58:40 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isupper check if character is upper case or lowercase
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = "A";
+
+	if(isupper(ch))
+	{
+		printf(" is uppercase chatacter");
+	}
+	else if(islower(ch))
+	{
+		printf(" is lowercase character");
+	}
+	else
+	{
+		pritf(" is not an character.");
+	}
+
+	return 0;
+}
