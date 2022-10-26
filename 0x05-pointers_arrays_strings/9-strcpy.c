976593707d4acff869fbab1ee08081e4e0809b49
Vuyani-Magibisela <vuyani.magibisela@live.com>
2022-10-25 19:39:15 -0700

    corrections

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,13 +6,16 @@
  * @src: copy from
  * Return: string
  */
+
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int index = 0;
 
-	for (; src[i] != '\0'; i++)
+	while (src[index])
 	{
-		dest[i] = src[i];
+		dest[index] = src[index];
+		index++;
 	}
+
 	return (dest);
 }
