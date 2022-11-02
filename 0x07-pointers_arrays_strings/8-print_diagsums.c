dbd8084c0884cb745741a7fbc8a8e4748b6a5709
Vuyani-Magibisela <vuyani.magibisela@live.com>
2022-11-01 20:28:32 -0700

    Squre matrix fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,19 +11,12 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
-	int c = 0;
-	int d = 0;
+	int i, sumx, sumy;
 
-	for (i = 0; i < size; i++)
-	{
-		c = c + *(a + i * sizeof(int));
-	}
-
-	for (j = 0; j < size; j++)
-
-	{
-		d = d + *(a + (size * j) + (size - j - 1));
-	}
-	printf("%d, %d\n", c, d);
+	sumx = sumy = 0;
+	for (i = 0; i < (size * size); i += size + 1)
+		sumx += a[i];
+	for (i = size - 1; i < (size * size - 1); i += size - 1)
+		sumy += a[i];
+	printf("%d, %d\n", sumx, sumy);
 }
